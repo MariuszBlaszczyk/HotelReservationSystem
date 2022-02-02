b2cb4a83dd91301df1f05c8389c80b2ebcc8c2d1
Mariusz  Błaszczyk <45122048+MariuszBlaszczyk@users.noreply.github.com>
2022-02-02 14:04:02 +0100

    if-else if-else

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        String hotelName = "Overlook Hotel";
+        int currentSystemVersion = 1;
+        boolean isDeveloperVersion = true;
+
+        System.out.print("Welcome to the reservation system for the ");
+        System.out.println(hotelName);
+        System.out.print("Current system version: ");
+        System.out.println(currentSystemVersion);
+        System.out.print("Development version: ");
+        System.out.println(isDeveloperVersion);
+
+        System.out.println("\n=========================\n");
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Add new guest.");
+        System.out.println("2. Add new room.");
+        System.out.println("3. Search for a guest.");
+        System.out.println("Select the option: ");
+
+        int option = input.nextInt();
+
+        if (option == 1) {
+            System.out.println("Selected option 1.");
+        } else if (option == 2) {
+            System.out.println("Selected option 2.");
+        } else if (option == 3) {
+            System.out.println("Selected option 3.");
+        } else {
+            System.out.println("Incorrect choice");
+        }
+
+
     }
 }
+
