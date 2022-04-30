7d2a7b48576c6d9028cb8886a7ee8d139b6773e5
Mariusz  Błaszczyk <45122048+MariuszBlaszczyk@users.noreply.github.com>
2022-04-30 09:02:35 +0200

    Add RoomTest

--- src/test/java/com/app/domain/room/RoomTest.java
+++ src/test/java/com/app/domain/room/RoomTest.java
@@ -13,58 +13,73 @@ public class RoomTest {
 
     @Test
     public void toCSVTest() {
-        List<BedType> bedTypeList = Arrays.asList(BedType.values());
 
+        //given
+        List<BedType> bedTypeList = Arrays.asList(BedType.values());
         Room room = new Room(1, 10, bedTypeList);
 
-        String csvTemplate = "1,10,Single#Double#Kingsize" + System.getProperty("line.separator");
+        //when
         String createdCSV = room.toCSV();
 
+        //then
+        String csvTemplate = "1,10,Single#Double#Kingsize" + System.getProperty("line.separator");
         assertEquals(csvTemplate, createdCSV, "Comparison of the generated CSV formats");
+
     }
 
 
     @Test
     public void toCVSWithNullBedList() {
+
+        //given
         Room room = new Room(1, 10, null);
 
-        assertNotNull(room.getBeds());
-
-        String csvTemplate = "1,10," + System.getProperty("line.separator");
-
+        //when
         String createdCSV = room.toCSV();
 
+        //then
+        assertNotNull(room.getBeds());
+        String csvTemplate = "1,10," + System.getProperty("line.separator");
         assertEquals(csvTemplate, createdCSV, "Comparison of generated CSV formats when bed list == null");
     }
 
 
     @Test
     public void toDTOTest() {
-        List<BedType> bedTypeList = Arrays.asList(BedType.values());
 
+        //given
+        List<BedType> bedTypeList = Arrays.asList(BedType.values());
         Room room = new Room(1, 10, bedTypeList);
 
+        //when
         RoomDTO roomDTO = room.generateDTO();
 
+        //then
         assertEquals(roomDTO.getId(), 1);
         assertEquals(roomDTO.getNumber(), 10);
         assertEquals(roomDTO.beds(), "Single,Double,Kingsize");
         assertEquals(roomDTO.bedsCount(), 3);
         assertEquals(roomDTO.roomSize(), 7);
+
     }
 
 
     @Test
     public void toDTOFromRoomWithNullBedsList() {
+
+        //given
         Room room = new Room(1, 10, null);
 
+        //when
         RoomDTO roomDTO = room.generateDTO();
 
+        //then
         assertEquals(roomDTO.getId(), 1);
         assertEquals(roomDTO.getNumber(), 10);
         assertEquals(roomDTO.beds(), "");
         assertEquals(roomDTO.bedsCount(), 0);
         assertEquals(roomDTO.roomSize(), 0);
+
     }
 
 }
